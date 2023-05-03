1eb710cb4e39a199b3f666eed0fa0878ea6ad84c
Anshul Karanam <125226129+anshulck03@users.noreply.github.com>
2023-05-02 20:47:34 -0700

    Update TeamAnalyzer.py

--- Python/TeamAnalyzer.py
+++ Python/TeamAnalyzer.py
@@ -1,34 +1,59 @@
-import sqlite3  # This is the package for all sqlite3 access in Python
-import sys      # This helps with command-line parameters
+import sqlite3
+import sys
 
 # All the "against" column suffixes:
-types = ["bug","dark","dragon","electric","fairy","fight",
-    "fire","flying","ghost","grass","ground","ice","normal",
-    "poison","psychic","rock","steel","water"]
+types = ["bug", "dark", "dragon", "electric", "fairy", "fight",
+         "fire", "flying", "ghost", "grass", "ground", "ice", "normal",
+         "poison", "psychic", "rock", "steel", "water"]
 
 # Take six parameters on the command-line
-if len(sys.argv) < 6:
+if len(sys.argv) != 7:
     print("You must give me six Pokemon to analyze!")
     sys.exit()
 
 team = []
+conn = sqlite3.connect('pokemon.db')
+cur = conn.cursor()
+
 for i, arg in enumerate(sys.argv):
     if i == 0:
         continue
 
-    # Analyze the pokemon whose pokedex_number is in "arg"
+    if arg.isdigit():
+        cur.execute("SELECT * FROM pokemon WHERE pokedex_number=?", (arg,))
+    else:
+        cur.execute("SELECT * FROM pokemon WHERE name=?", (arg.capitalize(),))
 
-    # You will need to write the SQL, extract the results, and compare
-    # Remember to look at those "against_NNN" column values; greater than 1
-    # means the Pokemon is strong against that type, and less than 1 means
-    # the Pokemon is weak against that type
+    pokemon = cur.fetchone()
+
+    if not pokemon:
+        print(f"Pokedex number or Pokemon name {arg} not found.")
+        sys.exit()
+
+    strong_against = []
+    weak_against = []
+
+    for t in types:
+        against_value = pokemon[types.index(t) + 1]
+        if against_value > 1:
+            strong_against.append(t)
+        elif against_value < 1:
+            weak_against.append(t)
+
+    print(f"Analyzing {arg}")
+    print(f"{pokemon[1]} ({pokemon[2]} {pokemon[3]}) is strong against {strong_against} but weak against {weak_against}")
+    team.append(pokemon)
 
 answer = input("Would you like to save this team? (Y)es or (N)o: ")
 if answer.upper() == "Y" or answer.upper() == "YES":
     teamName = input("Enter the team name: ")
 
-    # Write the pokemon team to the "teams" table
+    for pokemon in team:
+        cur.execute("INSERT INTO teams (team_name, pokedex_number) VALUES (?, ?)", (teamName, pokemon[0]))
+
+    conn.commit()
     print("Saving " + teamName + " ...")
 else:
     print("Bye for now!")
 
+conn.close()
